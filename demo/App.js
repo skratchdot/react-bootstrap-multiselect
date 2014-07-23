a0d95332090f048760465457f7acd3496723c988
skratchdot <git@skratchdot.com>
2014-07-23 16:09:12 -0600

    fixing bug in demo

--- demo/App.js
+++ demo/App.js
@@ -4,8 +4,6 @@ var React = require('react');
 var Grid = require('react-bootstrap').Grid;
 var Row = require('react-bootstrap').Row;
 var Col = require('react-bootstrap').Col;
-var Button = require('react-bootstrap').Button;
-var Glyphicon = require('react-bootstrap').Glyphicon;
 var Header = require('./Header');
 var Multiselect = require('../index.js');
 var fileContent = require('./AppContent').content;
@@ -18,9 +16,9 @@ var App = React.createClass({
 		}
 		return {
 			groups: [
-				{title:'Group One',children:[{value:'One'},{value:'Two'},{value:'Three'},{value:'Four',label:'Four Label'}]},
-				{title:'Group Two',children:[{value:'One'},{value:'Two'},{value:'Three'},{value:'Four',label:'Four Label'}]},
-				{title:'Group Three',children:[{value:'One'},{value:'Two'},{value:'Three'},{value:'Four',label:'Four Label'}]}
+				{title:'Group One',children:[{value:'1-One'},{value:'1-Two'},{value:'1-Three'},{value:'1-Four',label:'Four Label'}]},
+				{title:'Group Two',children:[{value:'2-One'},{value:'2-Two'},{value:'2-Three'},{value:'2-Four',label:'Four Label'}]},
+				{title:'Group Three',children:[{value:'3-One'},{value:'3-Two'},{value:'3-Three'},{value:'3-Four',label:'Four Label'}]}
 			],
 			large: large,
 			list: [{value:'One',selected:true},{value:'Two'},{value:'Three'},{value:'Four',label:'Four Label'}]
