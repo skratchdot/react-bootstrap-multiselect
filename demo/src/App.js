96a9f04553ebad94d3f61395ce9f8925b8561211
Raghav Sharma <Raghav_ftw@hotmail.com>
2016-05-18 14:01:49 +0530

    Update App.js
    use option.label to set title for optgroup not option.title

--- demo/src/App.js
+++ demo/src/App.js
@@ -14,9 +14,9 @@ var App = React.createClass({
 		}
 		return {
 			groups: [
-				{title:'Group One',children:[{value:'1-One'},{value:'1-Two'},{value:'1-Three'},{value:'1-Four',label:'Four Label'}]},
-				{title:'Group Two',children:[{value:'2-One'},{value:'2-Two'},{value:'2-Three'},{value:'2-Four',label:'Four Label'}]},
-				{title:'Group Three',children:[{value:'3-One'},{value:'3-Two'},{value:'3-Three'},{value:'3-Four',label:'Four Label'}]}
+				{label:'Group One',children:[{value:'1-One'},{value:'1-Two'},{value:'1-Three'},{value:'1-Four',label:'Four Label'}]},
+				{label:'Group Two',children:[{value:'2-One'},{value:'2-Two'},{value:'2-Three'},{value:'2-Four',label:'Four Label'}]},
+				{label:'Group Three',children:[{value:'3-One'},{value:'3-Two'},{value:'3-Three'},{value:'3-Four',label:'Four Label'}]}
 			],
 			large: large,
 			list: [{value:'One',selected:true},{value:'Two'},{value:'Three'},{value:'Four',label:'Four Label'}]
